01b15a05f3339241d129916db23bb0f21265816b
Dan Vallejo <danv@cstructor.com>
2021-07-08 19:18:37 -0700

    add RsvpForm

---
+++ HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+
+    }
+}
